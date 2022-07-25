b6c3ffba40e91ae922ce312a5aee21e56c5f6528
DavidC <david.cisar@gmx.de>
2022-07-25 16:50:15 +0200

    added getters and setters and addingTechnology to Project funtionality

--- Backend/src/main/java/io/dcisar/backend/project/Project.java
+++ Backend/src/main/java/io/dcisar/backend/project/Project.java
@@ -23,6 +23,8 @@ public class Project {
     )
     private List<Technology> technologiesInProject = new ArrayList<Technology>();
 
+    public Project() {}
+
     public Project(
             String name,
             String description,
@@ -30,5 +32,49 @@ public class Project {
         this.name = name;
         this.description = description;
         this.projectContext = projectContext;
+    }
+
+    public void addTechnologyToProject(Technology technology) {
+        technologiesInProject.add(technology);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProjectContext() {
+        return projectContext;
+    }
+
+    public void setProjectContext(String projectContext) {
+        this.projectContext = projectContext;
+    }
+
+    public List<Technology> getTechnologiesInProject() {
+        return technologiesInProject;
+    }
+
+    public void setTechnologiesInProject(List<Technology> technologiesInProject) {
+        this.technologiesInProject = technologiesInProject;
     }
 }
