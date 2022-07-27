39e462dc10eb8a3f2cae73361d202f36f5ad40bf
DavidC <david.cisar@gmx.de>
2022-07-27 11:50:38 +0200

    added language entity

--- Backend/src/main/java/io/dcisar/backend/technology/LanguageRepository.java
+++ Backend/src/main/java/io/dcisar/backend/technology/LanguageRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TechnologyRepository extends JpaRepository<Technology, Long> {
+public interface LanguageRepository extends JpaRepository<Language, Long> {
 
 }
