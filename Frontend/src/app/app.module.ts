1e4a246fc86c3170181352d8f35334d8b7fece33
DavidC <david.cisar@gmx.de>
2022-07-27 17:03:08 +0200

    testing background colors

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
