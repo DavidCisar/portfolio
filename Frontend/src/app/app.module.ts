9fff5aef821604116dba6fa1b38edfb980fcb714
DavidC <david.cisar@gmx.de>
2022-08-17 10:15:45 +0200

    added admin component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
