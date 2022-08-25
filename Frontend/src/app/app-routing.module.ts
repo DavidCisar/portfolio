1ae4349cf8521dc99033c759e090b7f3bcced3ef
DavidC <david.cisar@gmx.de>
2022-08-25 08:46:06 +0200

    init about & admin component.html

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
--- Frontend/src/app/components/admin/admin.component.ts
+++ Frontend/src/app/components/admin/admin.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { IProject } from 'src/app/model/project';
+import { ILanguage }  from 'src/app/model/language';
+import { IFramework } from 'src/app/model/framework';
+import { ITopic } from 'src/app/model/topic';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +14,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  projects: IProject[] = [];
+  languages: ILanguage[] = [];
+  frameworks: IFramework[] = [];
+  topics: ITopic[] = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+
+    this.http.get<IProject[]>('api/v1/projects')
+      .subscribe((projects: IProject[]) => this.projects = projects)
+
+    this.http.get<ILanguage[]>('api/v1/languages')
+      .subscribe((languages: ILanguage[]) => this.languages = languages)
+
+    this.http.get<IFramework[]>('api/v1/frameworks')
+      .subscribe((frameworks: IFramework[]) => this.frameworks = frameworks)
+
+    this.http.get<ITopic[]>('api/v1/topics')
+      .subscribe((topics: ITopic[]) => this.topics = topics)
   }
 
 }
