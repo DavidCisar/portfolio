dbbc3efd680f10eec0ecfa73a76dfc85707235aa
DavidC <david.cisar@gmx.de>
2022-09-17 10:01:12 +0200

    added funtionality to track failed or successful login attempts

---
+++ Backend/src/main/java/io/dcisar/backend/config/security/AuthenticationSuccessListener.java
@@ -0,0 +1,25 @@
+package io.dcisar.backend.config.security;
+
+import io.dcisar.backend.user.LoginAttemptService;
+import io.dcisar.backend.user.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationSuccessListener {
+
+    private final LoginAttemptService loginAttemptService;
+
+    @EventListener
+    public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+        if (principal instanceof UserPrincipal) {
+            UserPrincipal user = (UserPrincipal) event.getAuthentication().getPrincipal();
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
+        }
+    }
+
+}
