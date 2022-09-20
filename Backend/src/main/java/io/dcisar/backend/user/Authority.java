250b423cbf6a16430485e03cbbbdb53361d6c5d9
DavidC <david.cisar@gmx.de>
2022-09-20 18:14:57 +0200

    admin has authority to delete user

--- Backend/src/main/java/io/dcisar/backend/user/Authority.java
+++ Backend/src/main/java/io/dcisar/backend/user/Authority.java
@@ -2,5 +2,5 @@ package io.dcisar.backend.user;
 
 public class Authority {
     public static final String[] USER_AUTHORITIES = {"rating:create", "rating:update", "rating:delete"};
-    public static final String[] ADMIN_AUTHORITIES = {"rating:delete", "admin:all"};
+    public static final String[] ADMIN_AUTHORITIES = {"rating:delete", "admin:all", "user:delete"};
 }
