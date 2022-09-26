fa77cd0909c483d158b1ad23428b7412e093fd65
DavidC <david.cisar@gmx.de>
2022-09-26 15:15:25 +0200

    skills visibility init

--- Frontend/src/app/components/skills/skills.component.ts
+++ Frontend/src/app/components/skills/skills.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ITopic } from 'src/app/model/topic';
+import { IFramework } from 'src/app/model/framework';
+import { ILanguage } from 'src/app/model/language';
 
 @Component({
   selector: 'app-skills',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  languages: ILanguage[] = [];
+  frameworks: IFramework[] = [];
+  topics: ITopic[] = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<IFramework[]>("/api/v1/frameworks")
+      .subscribe((frameworks: IFramework[]) => this.frameworks = frameworks);
+    this.http.get<ILanguage[]>("/api/v1/languages")
+      .subscribe((languages: ILanguage[]) => this.languages = languages);
+    this.http.get<ITopic[]>("/api/v1/topics")
+      .subscribe((topics: ITopic[]) => this.topics = topics);
+  }
+
+  concatenateLanguage(language: string) : string {
+      return "programming lang-" + language.toLowerCase();
   }
 
 }
