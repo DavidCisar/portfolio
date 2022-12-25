da3ccea7d0a2851b8ac9ba467ecedf3bdb96a343
DavidC <david.cisar@gmx.de>
2022-12-25 16:43:08 +0100

    updated model

--- Frontend/src/app/components/room/Utils/Assets.ts
+++ Frontend/src/app/components/room/Utils/Assets.ts
@@ -8,5 +8,10 @@ export default [
     name: 'trees',
     type: 'glbModel',
     path: './assets/three/models/trees.glb'
+  },
+  {
+    name: 'particles',
+    type: 'glbModel',
+    path: './assets/three/models/particles.glb'
   }
 ]
